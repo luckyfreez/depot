569f94f1740103fdc719fad0b2b2d4fb8cb8b9b1
Lucky Zhang <lucky@phonetab.com>
2011-06-02 12:14:16 -0400

    Scaffold, Validation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '7ecbf94ed04e68945750d52ea9243b71eaec669ca435467859ec74bf55d9126932b8cc7fb3befd97c6ccf4d86907d539bc7157fb0096900f90f6f5ccf075c548'
