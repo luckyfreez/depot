e3423f04ad95dbaa23ebc11bc27cc6d890ccb751
Lucky Zhang <lucky@phonetab.com>
2011-06-02 18:19:28 -0400

    model test fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Depot::Application.routes.draw do
+  get "store/index"
+
   resources :products
 
   # The priority is based upon order of creation:
@@ -53,7 +55,8 @@ Depot::Application.routes.draw do
   # root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
-
+  
+  root :to => 'store#index', :as => 'store'
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
 def new_product(attrs={})
-  attributes = { :title => "something",
-                 :price => 9,
-                 :description => "I love you",
-                 :image_url => "6.jpg"}.merge(attrs)
+  attributes = {:title => "Some title",
+              :price => 3,
+              :description => "I love you",
+              :image_url => "6.jpg"}.merge(attrs)
   Product.new(attributes)
 end
 
@@ -13,21 +13,20 @@ class ProductTest < ActiveSupport::TestCase
   test "image url" do
     validUrl = %w{blah.jpg b.png i.gif gif.jpg}
     invalidUrl = %w{blah.pgn jpg.1 blajpg}
-    validUrl.each do |url|
-      product1 = new_product(:image_url => url)
-      assert product1.valid?, "product with image_url = #{url} shouldn't be invalid"
-    end
+    validUrl.each {|url| 
+      assert new_product(:image_url => url).valid?, "#{url} shouldn't be invalid"
+    }
     invalidUrl.each do |url|
-      assert new_product(:image_url => url).invalid?, "product with image_url = #{url} shouldn't be valid"
+      assert new_product(:image_url => url).invalid?, "#{url} shouldn't be valid"
     end
   end
 
   test "uniqueness of title" do
     product1 = new_product
     product1.save
-    product2 = new_product(:title => product1.title)
-    product3 = new_product(:title => "something else")
-    assert product2.invalid?, "Products with same titles cannot be created."
-    assert product3.valid?, "Products with unique titles should have been valid to create."
+    product2 = new_product
+    product3 = new_product(:title => "some else title")
+    assert product2.invalid?, "product with same title shouldn't be valid to create"
+    assert product3.valid?, "prodcut with unique title should be valid to create"
   end
 end
